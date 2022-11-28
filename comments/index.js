c4507010076d1ee2ea288d9a2fe505586eb7896a
Also Ailton <ailton_gulmanelli@hotmail.com>
2022-11-28 18:18:56 -0300

    add  status field at comments and query files

--- comments/index.js
+++ comments/index.js
@@ -22,7 +22,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status:'pending' });
 
     commentsByPostId[req.params.id] = comments;
 
@@ -33,7 +33,8 @@ app.post('/posts/:id/comments', async (req, res) => {
                 commentId,
                 content,
             postId:
-                req.params.id
+                req.params.id,
+            status: 'pending' 
         }
     })
 
